6da00d794a30d29cebd87914d1f236b2b7958b5e
juan <michelle.70623@gmail.com>
2021-12-06 14:23:02 +0800

    function design, toast, dialog

--- app/src/main/java/com/tom/guess/MainActivity.kt
+++ app/src/main/java/com/tom/guess/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.tom.guess
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,13 +16,13 @@ class MainActivity : AppCompatActivity() {
         Log.d("MainActivity", "secret:" + secretNumber.secret)
     }
 
-    fun check(view : View) {
+    fun check(view: View) {
         val n = number.text.toString().toInt()
         println("number: $n")
         Log.d("MainActivity", "number:" + n)
         val diff = secretNumber.validate(n)
         var message = "Yes, you get it"
-        if (diff  < 0) {
+        if (diff < 0) {
             message = "Bigger"
         } else if (diff > 0) {
             message = "Smaller"
